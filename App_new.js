011307116286056fac8e761a34d69f08963262b9
mennasingergy <104042069+mennasingergy@users.noreply.github.com>
2022-06-07 13:32:00 +0200

    Add files via upload

--- App_new.js
+++ App_new.js
@@ -40,31 +40,12 @@ export const updateShipment = async (req, res) => {
         // update the database with new
         const x = order_id;
         const currentShipmentStatus = shipment.shipment_status;
-        var nextShipmentStatus;
-        switch(currentShipmentStatus){
-            case "CREATED": nextShipmentStatus = "SHIPPED";
-            break;
-            case "SHIPPED": if (x % 2 ===0) {
-                nextShipmentStatus = "CANCELLED";
-            } else{
-                nextShipmentStatus = "DELEVIRED";
-            } ;
-            break;
+        const nextShipmentStatus={
+             "CREATED": 'SHIPPED',
+            "SHIPPED": "DELIVERED"
         }
-        //     CREATED: 'SHIPPED',
-        // //    SHIPPED: 'DELIVERED'
-        // [currentShipmentStatus];
-        // if(x%2===0){
-        //     const cancel_Shipment = {
-        //         SHIPPED: 'CANCELED'
-        //     }[currentShipmentStatus];
-        // }
-        // else {
-        //     const deliver_Shipment = {
-        //         SHIPPED: 'DELIVERED'
-        //     }[currentShipmentStatus];
-        // }
-
+        [currentShipmentStatus];
+        
         const updatedDocument = await Shipment.updateOne({ order_id: order_id }, { shipment_status: nextShipmentStatus }, { returnDocument: true });
         return res.status(200).send({ body: updatedDocument, message: 'Successfully updated order status' });
 
@@ -72,4 +53,24 @@ export const updateShipment = async (req, res) => {
         console.log('[updateShipment] e', e)
     }
 }
+export const cancelShipment = async (req, res) => {
+    try {
+        console.log('[cancelShipment body]', req.body)
+        const { order_id } = req.body;
+        if (!order_id) return res.status(403).send('order_id is required');
+
+        const shipment = await Shipment.findOne({ order_id: order_id });
+        if (!shipment) return res.status(403).send('Shipment does not exist');
+
+        if(shipment.shipment_status === 'CREATED') {
+            await Shipment.updateOne({ order_id }, { shipment_status: 'CANCELED' });
+            return res.status(200).send({ body: 'CANCELED', message: 'Your Shipment has been canceled' });
+        }
+        return res.status(200).send({ body: shipment.shipment_status, message: 'Cannot cancel this shipments' });
+    }
+    catch (e) {
+        console.log('[cancelShipment] e', e)
+    }
+}
+
 
